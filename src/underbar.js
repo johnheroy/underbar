db767cfded0fe65a108b8fc52a694c0d5c47bfda
John Heroy <johnheroy@gmail.com>
2014-08-06 21:43:15 +0800

    Implement _.some()

--- src/underbar.js
+++ src/underbar.js
@@ -209,6 +209,12 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if (iterator === undefined) {
+      var antiIterator = function(item) { return !item; };
+    } else {
+      var antiIterator = function(item) { return !iterator(item); };  
+    }
+    return !_.every(collection, antiIterator);
   };
 
 
